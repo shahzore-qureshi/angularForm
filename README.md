57f86e98c2dc0f0f047776857d5a99f6bfd907b8
Shahzore Qureshi <squreshi@sapient.com>
2018-03-18 16:10:21 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+To start project, run the commands "npm install" and "npm start".
+Project will be displayed at the url http://localhost:3000/.
---
+++ public/js/components.js
@@ -0,0 +1,49 @@
+angular
+.module('components', [])
+.directive('screens', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {},
+    controller: function($scope, $element) {
+      var screens = $scope.screens = [];
+
+      $scope.select = function(screen) {
+        angular.forEach(screens, function(screen) {
+          screen.selected = false;
+        });
+        screen.selected = true;
+      }
+
+      this.addScreen = function(screen) {
+        if (screens.length == 0) $scope.select(screen);
+        screens.push(screen);
+      }
+    },
+    template:
+      '<div class="screens">' +
+        '<ul class="nav nav-tabs">' +
+          '<li ng-repeat="screen in screens" class="nav-item">'+
+            '<a href="" ng-click="select(screen)" ng-class="{active:screen.selected}">{{screen.title}}</a>' +
+          '</li>' +
+        '</ul>' +
+        '<div class="tab-content" ng-transclude></div>' +
+      '</div>',
+    replace: true
+  };
+})
+.directive('screen', function() {
+  return {
+    require: '^screens',
+    restrict: 'E',
+    transclude: true,
+    scope: { title: '@' },
+    link: function(scope, element, attrs, screensController) {
+      screensController.addScreen(scope);
+    },
+    template:
+      '<div class="tab-pane" ng-class="{active: selected}" ng-transclude>' +
+      '</div>',
+    replace: true
+  };
+})
